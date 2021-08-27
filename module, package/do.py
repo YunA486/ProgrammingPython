ad49b6fa84d98800877c93013ea7c3bf7f2b16fb
joyun <w2039@e-mirim.hs.kr>
2021-08-27 10:18:50 +0900

    module

--- module, package/do.py
+++ module, package/do.py
@@ -1,5 +1,28 @@
 import my_module as mm
-
 mm.지원이()
-
 print(mm.date)
+
+import tour.gangreung as ga
+gp = ga.GangreungPackage()
+print(gp)
+
+from tour import gangreung
+gp = gangreung.GangreungPackage()
+print(gp)
+
+from tour.gangreung import GangreungPackage
+gp = GangreungPackage()
+print(gp)
+
+from tour import ilsan
+ilsan.이지()
+
+import tour.ilsan
+tour.ilsan.이지()
+
+from tour.ilsan import 이지
+이지()
+
+from tour import *
+ilsan.이지()
+gp = gangreung.GangreungPackage()
--- nadocoding/2314_practice.py
+++ nadocoding/2314_practice.py
@@ -1163,8 +1163,8 @@ from travel import *
 trip_to = thailand.ThailandPackage()
 trip_to.detail()
 
-import  inspect
-import  random
+import inspect
+import random
 print(inspect.getfile(random))
 print(inspect.getfile(thailand))
 
