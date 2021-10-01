d7c52a4568c836ddc65e3bfb397566a79ae6e561
YunA486 <w2039@e-mirim.hs.kr>
2021-10-01 10:01:10 +0900

    save text encoding

--- file handler/save_text.py
+++ file handler/save_text.py
@@ -1,10 +1,7 @@
+f = open('text.txt', 'w', encoding='utf-8')
 
-try:
-    f = open('text.txt', 'w')
+f.write('hello')
+f.write('\n')
+f.write('월드')
 
-    f.write('hello')
-    f.write('월드')
-except:
-    pass
-finally:
-    f.close()
+f.close()
