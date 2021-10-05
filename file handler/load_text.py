c0709dff86fb6a4fa52770b430975710d1b224ac
YunA486 <w2039@e-mirim.hs.kr>
2021-10-05 11:26:51 +0900

    load text, read(), readline(), readlines()

--- file handler/load_text.py
+++ file handler/load_text.py
@@ -1,9 +1,28 @@
-f = open('text.txt', 'r', encoding='utf-8')
+print('한꺼번에 전체 읽기')
 
+# f = open('text.txt', 'r', encoding='utf-8')
+# data = f.read()
+# f.close()
+# print(data)
 
-data = f.read()
+# encoding='utf-8' 넣으니까 에러..왜?
 
-
-f.close()
-
+with open('text.txt', 'r') as f:
+    data = f.read()
 print(data)
+
+print('한 줄씩 읽기')
+with open('text.txt', 'r') as f:
+    while True:
+        line = f.readline()     # line: 'hello\n'
+        if line == '':      # 파일에서 읽어올 내용 없으면, 끝
+            break
+        print(line.rstrip())    # print('hello')
+
+print('한꺼번에 전체 읽어서, 한 줄씩 리스트')
+with open('text.txt', 'r') as f:
+    lines = f.readlines()
+    # print(lines)
+
+for line in lines:
+    print(line.rstrip())
