68a3fd93e51f1286f1885ca5e6b9a5ea609849a7
s2019w26 <s2016w26@e-mirim.hs.kr>
2021-10-29 10:20:03 +0900

    tictactoe game set Winner

--- tictactoe/tictactoc_game_engine.py
+++ tictactoe/tictactoc_game_engine.py
@@ -13,14 +13,28 @@ class TictactoeGameEngine:
     def set(self, row, col):
         self.board[self.SIZE*(row-1) + (col-1)] = self.turn
 
+    def position_to_index(self, row, col):
+        return self.SIZE * (row - 1) + (col - 1)
+
     def set_winner(self):
         # - 3줄
+        for row in range(1, 3+1):   # row : 1~3 반복
+            if self.board[self.position_to_index(row,1)] == self.board[self.position_to_index(row,2)] == self.board[self.position_to_index(row,3)] == self.turn:    # '.'일 때, 끝 안나게 하기
+                return self.turn
         # | 3줄
+        for col in range(1, 3+1):   # col : 1~3 반복
+            if self.board[self.position_to_index(1,col)] == self.board[self.position_to_index(2,col)] == self.board[self.position_to_index(3,col)] == self.turn:    # '.'일 때, 끝 안나게 하기
+                return self.turn
         # / 3줄
+        if self.board[self.position_to_index(1,1)] == self.board[self.position_to_index(2,2)] == self.board[self.position_to_index(3,3)] == self.turn:    # '.'일 때, 끝 안나게 하기
+            return self.turn
         # \ 3줄
-        return self.turn
+        if self.board[self.position_to_index(1,3)] == self.board[self.position_to_index(2,2)] == self.board[self.position_to_index(3,1)] == self.turn:    # '.'일 때, 끝 안나게 하기
+            return self.turn
         # 비기는 조건 : 다 채워졌을 때 위의 것에 해당 안됐을 때
-        return 'd' # draw
+        if not '.' in self.board:   # self.board 안에 '.'이 없다면
+            return 'd'  # draw
+
 
     def change_turn(self):
         # self.turn 'X'면  'O', 'O'면 'X'로 바꾸기
@@ -41,3 +55,10 @@ if __name__ == '__main__':
     print(game_engine.set_winner()) # 'X'
     game_engine.change_turn()
     print(game_engine.turn)     # 'O'
+    # 무승부 만들기, 확인하기
+    # game_engine.board('X', 'O', 'X', 'O', 'O', 'X', 'X', 'X', 'O')
+    # print(game_engine.set_winner()) # d
+    game_engine.set(1, 3)
+    game_engine.set(2, 2)
+    game_engine.set(3, 1)
+    print(game_engine.set_winner())     # '/'
